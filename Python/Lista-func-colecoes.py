a4ec81f8e4d650455e8bad3457df89416d5049b2
Maxwell Madureira Viana <126616347+vmaxbh@users.noreply.github.com>
2023-04-14 11:14:59 -0300

    estudo de listas

--- Python/Lista-func-colecoes.py
+++ Python/Lista-func-colecoes.py
@@ -8,4 +8,13 @@ print(lista1, end="")
 print(" Fizemos a juncao das duas listas")
 print(100 * "_")
 print(len(lista1))  # tamanho da lista com a funcao len
+print("--Indica quantidade de intes da lista1")
+print(100 * "_")
+print(sum(lista1[0:4]))
+print("--com a funcao sum somamos os intes de 0 a 4 da lista 1")
+print(100 * "_")
+lista3 = list(range(10))
+print(lista3, end="")
+print("--Aqui imprimimos suma lista de 0 a 9 a partir de um range")
+print(100 * "_")
 
---
+++ Python/lista-modif-indice.py
@@ -0,0 +1,10 @@
+lista = ["gato", "cachorro", "cavalo"]
+print(lista, end="")
+print("-Impressão lista inicial")
+print(100 * "_")
+print("vamos modificar o indice 'cachorro' para 'camelo'")
+print(100 * "_")
+lista[1] = "camelo"
+print(lista, end="")
+print("-Essa é a nossa lista modificada")
+
