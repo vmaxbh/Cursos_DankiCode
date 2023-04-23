6c16f3a1695f0256b345b9005d74ee5f83e4131f
Maxwell Madureira Viana <126616347+vmaxbh@users.noreply.github.com>
2023-04-23 18:37:18 -0300

    diversos

---
+++ Python/funcoes/recursao.py
@@ -0,0 +1,18 @@
+
+"""def reduzirNumero(numeroInt):
+    while numeroInt > 0:
+        print(numeroInt)
+        numeroInt-= 1
+
+
+reduzirNumero(5)"""
+
+
+"""def reduzirNumero(numeroInt):
+    print(numeroInt)
+    if numeroInt > 0:
+        reduzirNumero(numeroInt - 1)
+
+
+reduzirNumero(5)"""
+
